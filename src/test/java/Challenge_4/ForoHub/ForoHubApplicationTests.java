53c492f07e420839bd155d6bec4d2797b2b5897f
Joel-Paucar <paucar.pauca@gmail.com>
2025-01-17 17:12:44 -0500

    primer commit

---
+++ src/test/java/Challenge_4/ForoHub/ForoHubApplicationTests.java
@@ -0,0 +1,13 @@
+package Challenge_4.ForoHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForoHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
